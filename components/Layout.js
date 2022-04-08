2df3da4c4a67448dbd1d1dadf1f37961ce6528cb
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-04-09 03:39:31 +0530

    fix an id error in script tag

--- components/Layout.js
+++ components/Layout.js
@@ -34,8 +34,8 @@ const Layout = ({ component }) => {
             </MenuBarContext.Provider>
             <Script src="https://kit.fontawesome.com/8b16cac71e.js" crossorigin="anonymous"></Script>
 
-            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-            <Script strategy="lazyOnload">
+            <Script id="g_analytics_id" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+            <Script id="g_analytics_script" strategy="lazyOnload">
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
