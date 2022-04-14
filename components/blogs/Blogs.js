f57d9109bf1b34fd03c5c238e24d3cb31e74e2b1
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-04-14 08:22:25 +0530

    blogs redirect to hashnode blogs

--- components/blogs/Blogs.js
+++ components/blogs/Blogs.js
@@ -27,16 +27,16 @@ const Blogs = () => {
                 <Heading heading={"Read Blogs"} />
                 <div className="w-full my-10">
                     {blogsData.map((post, index) => (
-                        <Link key={index} href={post.url}>
+                        <Link key={index} href={`https://abhisblogs.hashnode.dev/${post.title.replaceAll(" ", "-").replaceAll(":", "").toLowerCase()}`}>
                             <a target={"_blank"}>
                                 <motion.div initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0, transition: { duration: 0.2, delay: index * 0.1 } }} exit={{ opacity: 0, y: -10, transition: { duration: 0.1, delay: index * 0.1 } }} key={index} className="flex flex-col justify-start w-[90%] mx-auto my-16 overflow-hidden rounded-md cursor-pointer md:w-full md:flex-row bg-darkBluePrimary">
                                     <div className="w-full md:w-[30rem] h-[15rem]">
                                         <img src={post.social_image} className="object-cover w-full h-full rounded-tl-md rounded-tr-md" alt="" />
                                     </div>
-                                    <div className="flex flex-col items-start justify-between w-full p-4 md:p-10 md:px-8">
+                                    <div className="flex flex-col items-start justify-around w-full p-4 md:py-4 md:px-6">
                                         <h2 className="pt-3 text-2xl text-cyanPrimary">{post.title}</h2>
-                                        <p className="my-3 text-base text-lightBlueSecondary">{post.description}</p>
-                                        <div className="flex flex-row flex-wrap items-start justify-start my-2 md:flex-nowrap">
+                                        <p className="mt-0 text-base text-lightBlueSecondary">{post.description}</p>
+                                        <div className="flex flex-row flex-wrap items-start justify-start md:flex-nowrap">
                                             {post.tag_list.map((tag, index) => (
                                                 <span key={index + 1} className="px-4 py-2 m-2 text-sm rounded-full font-codeText bg-cyanPrimary/10 text-cyanPrimary/90">
                                                     #{tag}
