874fcc1298aa00458ab702996e440f0c7cbfdf0e
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-11-03 15:42:49 +0530

    removed twitter card

--- components/Layout.js
+++ components/Layout.js
@@ -43,11 +43,11 @@ const Layout = ({ component, width }) => {
           itemProp="image"
           content="https://www.web-developer-abhi.me/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpic.7a6f735d.jpg&w=640&q=75"
         />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
+        <meta property="twitter:card" content="summary" />
+        {/* <meta
           property="twitter:image"
           content="https://hashnode.com/utility/r?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Funsplash%2FxkBaqlcqeb4%2Fupload%2Fv1660318045256%2FjFLm_-c9U.jpeg%3Fw%3D1200%26h%3D630%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp%26fm%3Dpng"
-        />
+        /> */}
         <meta
           property="twitter:title"
           content="MERN Stack Developer - Abhi Dadhaniya"
