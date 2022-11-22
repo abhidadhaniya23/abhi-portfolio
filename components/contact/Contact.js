9916776152e6770f793150e6bed1e55d36273c38
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-11-22 12:23:38 +0530

    emailJs integrated

--- components/contact/Contact.js
+++ components/contact/Contact.js
@@ -1,99 +1,141 @@
 import Heading from "../Heading";
 import { motion } from "framer-motion";
-import { useState } from "react";
-import ContactContext from "../../context/ContactContext";
-import axios from "axios";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [msg, setMsg] = useState("");
-    const [formSubmitted, setFormSubmitted] = useState(false);
-    const [dataReceived, setDataReceived] = useState(false);
-    const submitForm = (event) => {
-        event.preventDefault();
-        const data = { name, email, subject, msg };
-        const submitRequest = async () => {
-            setFormSubmitted(true);
-            const reqData = await fetch("api/contact", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json, text/plain, */*",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            }).then((res) => {
-                if (res.status === 200) {
-                    setFormSubmitted(false);
-                    setDataReceived(true);
-                    setName("");
-                    setEmail("");
-                    setSubject("");
-                    setMsg("");
-                }
-            });
-        };
-        submitRequest();
-    };
+  const form = useRef();
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [dataReceived, setDataReceived] = useState(false);
+  const submitForm = (event) => {
+    event.preventDefault();
+    setFormSubmitted(true);
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setFormSubmitted(false);
+          setDataReceived(true);
+          event.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
-    const animations = {
-        nameInput: {
-            initial: { opacity: 0, x: 20 },
-            exit: { opacity: 0, x: 10, transition: { duration: 0.3 } },
-            animate: { opacity: 1, x: 0, transition: { duration: 0.3, delay: 0.3 } },
-        },
-        emailInput: {
-            initial: { opacity: 0, x: -20 },
-            exit: { opacity: 0, x: -10, transition: { duration: 0.3 } },
-            animate: { opacity: 1, x: 0, transition: { duration: 0.3, delay: 0.3 } },
-        },
-        subjectInput: {
-            initial: { opacity: 0, y: -30 },
-            exit: { opacity: 0, y: -10, transition: { duration: 0.3 } },
-            animate: { opacity: 1, y: 0, transition: { duration: 0.3, delay: 0.4 } },
-        },
-        msgInput: {
-            initial: { opacity: 0, y: -30 },
-            exit: { opacity: 0, y: -10, transition: { duration: 0.3 } },
-            animate: { opacity: 1, y: 0, transition: { duration: 0.3, delay: 0.5 } },
-        },
-        btn: {
-            initial: { opacity: 0, y: -30 },
-            exit: { opacity: 0, y: -10, transition: { duration: 0.3 } },
-            animate: { opacity: 1, y: 0, transition: { duration: 0.3, delay: 0.6 } },
-        },
-    };
-    return (
-        <>
-            <ContactContext.Provider value={{ name, setName, email, setEmail, subject, setSubject, msg, setMsg }}>
-                <div className="px-7">
-                    <Heading heading={"Get In Touch"} />
-                    <div className="mt-10 font-bodyText">
-                        <form onSubmit={submitForm} className="flex flex-col items-center justify-center">
-                            <div className="flex flex-row items-center justify-center">
-                                <motion.input value={name} onChange={(e) => setName(e.target.value)} variants={animations.nameInput} initial="initial" animate="animate" exit="exit" className="w-full mr-2 input" type="text" placeholder="Full Name" required />
-                                <motion.input value={email} onChange={(e) => setEmail(e.target.value)} variants={animations.emailInput} initial="initial" animate="animate" exit="exit" className="w-full ml-2 input" type="email" placeholder="Email" required />
-                            </div>
-                            <motion.input value={subject} onChange={(e) => setSubject(e.target.value)} variants={animations.subjectInput} initial="initial" animate="animate" exit="exit" className="w-full m-4 input" type="text" placeholder="Subject" required />
-                            <motion.textarea value={msg} onChange={(e) => setMsg(e.target.value)} variants={animations.msgInput} initial="initial" animate="animate" exit="exit" className="w-full m-2 h-28 input" placeholder="Message" name="" required></motion.textarea>
-                            <motion.button variants={animations.btn} initial="initial" animate="animate" exit="exit" className="btn" type="submit">
-                                {formSubmitted ? (
-                                    <>
-                                        <div className="loader"></div>
-                                    </>
-                                ) : dataReceived ? (
-                                    "Message Sent"
-                                ) : (
-                                    "Send Message"
-                                )}
-                            </motion.button>
-                        </form>
-                    </div>
-                </div>
-            </ContactContext.Provider>
-        </>
-    );
+  const animations = {
+    nameInput: {
+      initial: { opacity: 0, x: 20 },
+      exit: { opacity: 0, x: 10, transition: { duration: 0.3 } },
+      animate: { opacity: 1, x: 0, transition: { duration: 0.3, delay: 0.3 } },
+    },
+    emailInput: {
+      initial: { opacity: 0, x: -20 },
+      exit: { opacity: 0, x: -10, transition: { duration: 0.3 } },
+      animate: { opacity: 1, x: 0, transition: { duration: 0.3, delay: 0.3 } },
+    },
+    subjectInput: {
+      initial: { opacity: 0, y: -30 },
+      exit: { opacity: 0, y: -10, transition: { duration: 0.3 } },
+      animate: { opacity: 1, y: 0, transition: { duration: 0.3, delay: 0.4 } },
+    },
+    msgInput: {
+      initial: { opacity: 0, y: -30 },
+      exit: { opacity: 0, y: -10, transition: { duration: 0.3 } },
+      animate: { opacity: 1, y: 0, transition: { duration: 0.3, delay: 0.5 } },
+    },
+    btn: {
+      initial: { opacity: 0, y: -30 },
+      exit: { opacity: 0, y: -10, transition: { duration: 0.3 } },
+      animate: { opacity: 1, y: 0, transition: { duration: 0.3, delay: 0.6 } },
+    },
+  };
+  return (
+    <>
+      <div className="px-7">
+        <Heading heading={"Get In Touch"} />
+        <div className="mt-10 font-bodyText">
+          <form
+            ref={form}
+            onSubmit={submitForm}
+            className="flex flex-col items-center justify-center"
+          >
+            <div className="flex flex-row items-center justify-center">
+              <motion.input
+                name="name"
+                variants={animations.nameInput}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                className="w-full mr-2 input"
+                type="text"
+                placeholder="Full Name"
+                required
+              />
+              <motion.input
+                name="email"
+                variants={animations.emailInput}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                className="w-full ml-2 input"
+                type="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <motion.input
+              name="subject"
+              variants={animations.subjectInput}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              className="w-full m-4 input"
+              type="text"
+              placeholder="Subject"
+              required
+            />
+            <motion.textarea
+              variants={animations.msgInput}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              className="w-full m-2 h-28 input"
+              placeholder="Message"
+              name="message"
+              required
+            ></motion.textarea>
+            <motion.button
+              variants={animations.btn}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              disabled={formSubmitted}
+              className={`btn ${formSubmitted && "cursor-not-allowed"} `}
+              type="submit"
+            >
+              {formSubmitted ? (
+                <>
+                  <div className="loader"></div>
+                </>
+              ) : dataReceived ? (
+                "Message Sent"
+              ) : (
+                "Send Message"
+              )}
+            </motion.button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
--- context/ContactContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const ContactContext = React.createContext();
-
-export default ContactContext;
